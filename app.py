68b6d383026c1432512950e4c697c1cd93188813
SUMAN BEHERA <beherasuman44444@gmail.com>
2026-05-18 07:26:47 +0530

    Add FastAPI prediction app

---
+++ app.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+
+from src.models.predict_model import ChurnPredictor
+
+
+app = FastAPI(
+    title="Customer Churn Prediction API",
+    description="API for predicting whether a customer will churn or not.",
+    version="1.0.0"
+)
+
+predictor = ChurnPredictor()
+
+
+class CustomerData(BaseModel):
+    gender: str
+    SeniorCitizen: int
+    Partner: str
+    Dependents: str
+    tenure: int
+    PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    MonthlyCharges: float
+    TotalCharges: float
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Customer Churn Prediction API is running"
+    }
+
+
+@app.post("/predict")
+def predict_churn(data: CustomerData):
+    input_df = pd.DataFrame([data.model_dump()])
+
+    prediction, probability = predictor.predict(input_df)
+
+    result = "Churn" if prediction[0] == 1 else "Not Churn"
+
+    return {
+        "prediction": int(prediction[0]),
+        "result": result,
+        "churn_probability": float(probability[0])
+    }
